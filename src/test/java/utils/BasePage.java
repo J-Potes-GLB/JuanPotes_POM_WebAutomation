b0bb3ca1306c88e951cc3f68041ad5d77d58b974
J-Potes-GLB <juan.potes@globant.com>
2024-02-22 12:59:27 -0500

    Added attributes and methods of the BasePage Class

--- src/test/java/utils/BasePage.java
+++ src/test/java/utils/BasePage.java
@@ -1,4 +1,34 @@
 package utils;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class BasePage {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    // Constructor of BasePage
+    public BasePage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+    }
+
+    // Wait for an element to be visible
+    public void waitUntilVisibleElement(WebElement element) {
+        getWait().until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    public WebDriverWait getWait() {
+        return wait;
+    }
 }
