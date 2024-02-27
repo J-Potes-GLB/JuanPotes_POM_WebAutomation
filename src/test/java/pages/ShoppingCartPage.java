7eb9c8c8fecf86c6ebb6f98cdae827d7f0a85a70
J-Potes-GLB <juan.potes@globant.com>
2024-02-27 10:10:22 -0500

    Added the removal of items and fixed the empty confirmation

--- src/test/java/pages/ShoppingCartPage.java
+++ src/test/java/pages/ShoppingCartPage.java
@@ -5,10 +5,15 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import utils.BasePage;
 
+import java.util.List;
+
 public class ShoppingCartPage extends BasePage {
     // id = "checkout"
     @FindBy(name = "checkout")
     private WebElement checkoutButton;
+
+    @FindBy(className = "cart_button")
+    private List<WebElement> removeButtons;
 
     @FindBy(className = "shopping_cart_badge")
     private WebElement cartItemsNumber;
@@ -23,7 +28,29 @@ public class ShoppingCartPage extends BasePage {
         return new CheckoutStepOnePage(getDriver());
     }
 
+    private void clickRemoveButton(int index){
+        waitUntilVisibleElement(this.removeButtons.get(index));
+        this.removeButtons.get(index).click();
+    }
+
+    public void removeItemsFromCart(int quantity){
+        System.out.println("Remove Buttons: " + this.removeButtons.size());
+        if(quantity > this.removeButtons.size()){
+            quantity = this.removeButtons.size();
+        }
+        if(quantity > 0){
+            for(int i = 0; i < quantity; i++){
+                clickRemoveButton(0);
+            }
+        }
+    }
+
     public boolean cartHasItems(){
-        return this.cartItemsNumber.isDisplayed();
+        if(this.removeButtons.size() > 0){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
