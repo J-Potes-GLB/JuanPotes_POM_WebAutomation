010b04a990a9615a129e5564ff2cf0d3194b6494
J-Potes-GLB <juan.potes@globant.com>
2024-02-26 23:15:29 -0500

    Implemented PurchaseProduct test until the item is added to the cart

--- src/test/java/tests/PurchaseProductTest.java
+++ src/test/java/tests/PurchaseProductTest.java
@@ -2,14 +2,21 @@ package tests;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+import pages.ItemDetailsPage;
 import utils.BaseTest;
 
 public class PurchaseProductTest extends BaseTest {
     @Test
     public void purchaseProduct(){
-//        WebDriver driver = new ChromeDriver();
-//        String url = "https://www.saucedemo.com/";
-//        driver.get(url);
+        // Check we are in the InventoryPage
+        softAssert.assertEquals(inventoryPage.getProductsTitleText(), "Products");
 
+        // Click on the item name and go to the ItemDetailsPage
+        ItemDetailsPage itemDetailsPage = inventoryPage.clickItemLink();
+
+        // Add the product to the cart
+        itemDetailsPage.clickAddToCartButton();
+
+        softAssert.assertAll();
     }
 }
